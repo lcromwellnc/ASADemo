0c407d39d73dc2ce6758e3a513db25e380735539
Larry Cromwell <lcromwellnc@gmail.com>
2023-07-25 14:37:30 -0400

    Update hcdemo.py
    Change the default port

--- hcdemo.py
+++ hcdemo.py
@@ -17,7 +17,7 @@ def getTip():
 
 
 if __name__ =='__main__':
-    port = os.environ.get('FLASK_PORT') or 3333
+    port = os.environ.get('FLASK_PORT') or 8081
     port = int(port)
   
     app.run(port=port,host='0.0.0.0')
